8e6fde8380f9c8ca52d10dd2cbf3a8b50012ab72
Lia Freitas Lobo <lia.lobo@gmail.com>
2023-09-30 16:21:56 -0300

    chore: create swagger initial config

---
+++ swagger.js
@@ -0,0 +1,18 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Backend Fase 01',
+    description:
+      'Documentação das APIs criadas para o sistema de gerenciamento de fast food',
+  },
+  host: 'localhost:8000',
+  schemes: ['http'],
+};
+
+const outputFile = './path/swagger-output.json';
+const endpointsFiles = ['./index.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+  require('./index.js');
+});
