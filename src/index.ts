64217b95097f52efed2437e153a9359743a0b7d8
Jackeline Nascimento <jackeline.nascimento@ifood.com.br>
2023-10-15 21:38:14 -0300

    refactor da estrutura

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Sistema Pedidos');
+});
+
 app.listen(PORT, () => {
   console.log(
     'Server is Successfully Running, and App is listening on port ' + PORT,
