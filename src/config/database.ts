d185f97b531758b23bb4492d4d50652ab000947f
Jackeline Nascimento <jackeline.nascimento@ifood.com.br>
2023-10-16 21:19:48 -0300

    ajustes

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,7 +6,7 @@ const sequelize = new Sequelize({
   port: 5432,
   username: 'user',
   password: 'password',
-  database: 'sistema-pedidos',
+  database: 'sistema-clientes-pedidos',
 });
 
 export default sequelize;
